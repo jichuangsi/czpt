c1a76f70821df11baaf91386137223537ce92746
jichuansi <6834403@qq.com>
2019-09-06 10:13:41 +0800

    项目结构

---
+++ czpt-server/src/main/java/cn/com/czpt/CzptServerApplication.java
@@ -0,0 +1,13 @@
+package cn.com.czpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzptServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzptServerApplication.class, args);
+	}
+
+}
